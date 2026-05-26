400f5b2b6625f6c62c73dc47fa1efd174b77a496
LBrink05 <LBrinkmannx5@gmail.com>
2026-05-26 15:59:25 +0200

    added phash filtering to get rid of duplicate frames.

--- src/dataset_preparation.py
+++ src/dataset_preparation.py
@@ -4,48 +4,64 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
 import os
-
 import time
+from collections import deque
+import sys
+
+from PIL import Image
+import imagehash
 
 # raw_data path
 RAW_DATA_PATH = Path("Data/unlabeled")
 DATA_PATH_STR = "Data/unlabeled"
-
 ALLOWED_FLAGS = ["a","s","d","f"] #Much, Some, Little, None
+
 
 #extracting image frames from videos
 while True:
     query = input("Do you wish to extract images from videos? (y/n)")
-    if query == yes or query == y:
+    if query == 'yes' or query == 'y':
+
         for file in RAW_DATA_PATH.rglob('*.mp4'):
             if file.is_file():
                 frameid = 0
                 videoid = file.name[:-4]
                 camid = file.parent.parent.name
-
                 video = cv2.VideoCapture(file)
-                while(True):
-            
-                    ret,frame = video.read()
+                recent_hashes = deque(maxlen=5)
+                filepath = DATA_PATH_STR + f"/{camid}/{videoid}/"
+                pathlib.Path(filepath).mkdir(parents=True, exist_ok=True)
 
-                    if ret:
-                        filepath = DATA_PATH_STR + f"/{camid}/" + f"{videoid}/"
-                        pathlib.Path(filepath).mkdir(parents=True, exist_ok=True)
-                        
-                        name = filepath + camid + "_" + videoid + "_" + str(frameid) + '.jpg'
-                        print ('Creating: ' + name)
-                        
-                        cv2.imwrite(name, frame)
+                #Remove any prior generated images in cami/videoi directory
+                for f in Path(filepath).glob('*'):
+                    if f.is_file():
+                        f.unlink()
 
-                        frameid += 1
-                    else:
+                while True:
+                    ret, frame = video.read()
+                    if not ret:
                         break
 
+                    pil_frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                    hash_o = imagehash.phash(pil_frame)
+
+                    if any(hash_o - h < 5 for h in recent_hashes):
+                        frameid += 1
+                        continue
+
+                    name = f"{filepath}{camid}_{videoid}_{frameid}.jpg"
+                    cv2.imwrite(name, frame)
+                    recent_hashes.append(hash_o)
+                    print('Extracting: ' + name)
+                    frameid += 1
                 
+
                 video.release()
                 cv2.destroyAllWindows()
-                break
-    elif query == no or query == n:
+                print(f"Successfully extraced frames from {camid} {videoid}")
+                sys.exit()
+
+    elif query == 'no' or query == 'n':
         break
 
 
