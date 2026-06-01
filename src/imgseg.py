624643f90cb001104def0a956197c94f084b12d4
LBrink05 <LBrinkmannx5@gmail.com>
2026-06-01 19:27:02 +0200

    worked on imgseg

--- src/imgseg.py
+++ src/imgseg.py
@@ -5,7 +5,6 @@ import cv2
 # We are tying to do deep feature clustering with Gaussian Mixture Model
 #used in order to fine-tune image segmentation used for detection purposes
 def demo(network, path):
-    print("demo")
 
     #Load Random Image
     data, labels = cnn.load_dataset(path)
@@ -23,14 +22,18 @@ def demo(network, path):
     out = network[4].forward(out) #ReLu
     out = network[5].forward(out) #MaxPool
 
+
     #Showing original sample image
 
+    print(f"Showing Original Image of shape {image.shape}.\n")
     cv2.namedWindow('Original Image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Original Image', 512, 512)
     cv2.imshow('Original Image', (image.transpose(1,2,0) * 255).astype('uint8'))
+
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
-
+    
+    
